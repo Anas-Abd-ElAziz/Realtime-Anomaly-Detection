3bf992198425c32b272a9fb6755b03b9a001902c
gamal-abdelhakm <101900854+gamal-abdelhakm@users.noreply.github.com>
2023-03-31 16:32:18 +0200

    responsive ui

--- Test.py
+++ Test.py
@@ -32,8 +32,10 @@ class Window(QMainWindow):
             "camera4": self.findChild(QCheckBox, "checkBox_4"),
             "camera5": self.findChild(QCheckBox, "checkBox_5"),
         }
-        self.yolo_model = YOLO("yolov8n.pt")
-        self.yolo_model.predict(cv2.imread('logo.ico'), classes=0, verbose=False)
+
+    def closeEvent(self, event):
+        self.started = False
+        event.accept()
 
 
     def initialize_button(self):
@@ -49,20 +51,20 @@ class Window(QMainWindow):
             self.started = True
             self.pushButton.setText('Stop')
 
-        self.liveViewCamera0 = threading.Thread(target=self.loadVideo,  args=('liveView_0',f"testVids//normal1.avi", self.checkboxes["camera0"].isChecked(), self.yolo_model))
+        self.liveViewCamera0 = threading.Thread(target=self.loadVideo,  args=('liveView_0',f"testVids//normal1.avi", self.checkboxes["camera0"].isChecked()))
         self.liveViewCamera0.start()
-        self.liveViewCamera1 = threading.Thread(target=self.loadVideo,  args=('liveView_1',f"testVids//anomaly1.mp4", self.checkboxes["camera1"].isChecked(), self.yolo_model))
+        self.liveViewCamera1 = threading.Thread(target=self.loadVideo,  args=('liveView_1',f"testVids//anomaly1.mp4", self.checkboxes["camera1"].isChecked()))
         self.liveViewCamera1.start()
-        self.liveViewCamera2 = threading.Thread(target=self.loadVideo,  args=('liveView_2',f"testVids//normal2.mp4", self.checkboxes["camera2"].isChecked(), self.yolo_model))
+        self.liveViewCamera2 = threading.Thread(target=self.loadVideo,  args=('liveView_2',f"testVids//normal2.mp4", self.checkboxes["camera2"].isChecked()))
         self.liveViewCamera2.start()      
-        self.liveViewCamera3 = threading.Thread(target=self.loadVideo,  args=('liveView_3',f"testVids//normal3.mp4", self.checkboxes["camera3"].isChecked(), self.yolo_model))
+        self.liveViewCamera3 = threading.Thread(target=self.loadVideo,  args=('liveView_3',f"testVids//normal3.mp4", self.checkboxes["camera3"].isChecked()))
         self.liveViewCamera3.start()   
-        self.liveViewCamera4 = threading.Thread(target=self.loadVideo,  args=('liveView_4',f"testVids//normal4.avi", self.checkboxes["camera4"].isChecked(), self.yolo_model))
+        self.liveViewCamera4 = threading.Thread(target=self.loadVideo,  args=('liveView_4',f"testVids//normal4.avi", self.checkboxes["camera4"].isChecked()))
         self.liveViewCamera4.start()
-        self.liveViewCamera5 = threading.Thread(target=self.loadVideo,  args=('liveView_5',f"testVids//normal5.avi", self.checkboxes["camera5"].isChecked(), self.yolo_model))
+        self.liveViewCamera5 = threading.Thread(target=self.loadVideo,  args=('liveView_5',f"testVids//normal5.avi", self.checkboxes["camera5"].isChecked()))
         self.liveViewCamera5.start()
 
-    def loadVideo(self, video_label, video_source, enabled, shared_yolo_model):
+    def loadVideo(self, video_label, video_source, enabled):
         if not enabled:
             return
 
@@ -70,11 +72,13 @@ class Window(QMainWindow):
         count = 0
         yolo_queue = Queue(maxsize=1)
         anomaly_queue = Queue(maxsize=1)
+        yolo_model = YOLO("yolov8n.pt")
+        yolo_model.predict(cv2.imread('logo.ico'), classes=0, verbose=False)
 
         def yolo_predict(image):
             nonlocal yolo_queue
             start_time = time.time()
-            num_objects = len(shared_yolo_model.predict(image, classes=0, verbose=False)[0])
+            num_objects = len(yolo_model.predict(image, classes=0, verbose=False)[0])
             end_time = time.time()
             print("Execution time:", end_time - start_time, "seconds")
             try:
@@ -94,7 +98,6 @@ class Window(QMainWindow):
         anomaly_score = 0
         frame_buffer = []
         current_prediction=-1
-        first_frame = True
         
         while vid.isOpened() and self.started:
             QtWidgets.QApplication.processEvents()
@@ -135,10 +138,11 @@ class Window(QMainWindow):
             text = "Anomaly State : " + str(anomaly_score)[:5] + " "+ anomaly_state
             image = ps.putBText(image, text, text_offset_x=30, text_offset_y=90, vspace=20, hspace=10, font_scale=1.0, background_RGB=(230, 230, 230), text_RGB=((10, 255, 10) if anomaly_state == 'Normal' else (255, 10, 10)))
             
+            label = self.centralwidget.findChild(QLabel, video_label)
             frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            frame = imutils.resize(frame, height=label.geometry().height(),width=label.geometry().width()-100)
             qimage = QImage(frame, frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format_RGB888)
-            pixmap = QPixmap.fromImage(qimage)
-            label = self.findChild(QLabel, video_label)
+            pixmap = QPixmap.fromImage(qimage)   
             label.setPixmap(pixmap)
             label.setScaledContents(True)
 
